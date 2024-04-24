bfb1c18d377e3cff7dc4d099355984de41873dca
Denis Pojar <denis.pojar@trimm.nl>
2024-04-24 11:50:59 +0200

    feat(#8): make the middleware to follow the traefik-plugin structure

--- api/v1/auth_api.go
+++ api/v1/auth_api.go
@@ -9,7 +9,6 @@ import (
 	"golang.org/x/oauth2"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"gopkg.in/square/go-jose.v2/jwt"
@@ -18,13 +17,7 @@ import (
 type AuthData struct {
 	ClientName   string `json:"clientName"`
 	ClientSecret string `json:"clientSecret"`
-}
-
-func NewAuthData() *AuthData {
-	return &AuthData{
-		ClientName:   os.Getenv("CLIENT_NAME"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-	}
+	ServerURL    string `json:"serverURL"`
 }
 
 type TokenData struct {
@@ -32,31 +25,39 @@ type TokenData struct {
 	ClientId string
 }
 
-func NewTokenData() *TokenData {
-	return &TokenData{}
-}
-
 type GraphQLClient struct {
 	client *graphql.Client
 }
 
-func NewGraphQLClient(tokenData *TokenData) *GraphQLClient {
-	token, err := tokenData.GetToken()
+func NewAuthData(clientName string, clientSecret string, serverURL string) *AuthData {
+	return &AuthData{
+		ClientName:   clientName,
+		ClientSecret: clientSecret,
+		ServerURL:    serverURL,
+	}
+}
+
+func NewTokenData() *TokenData {
+	return &TokenData{}
+}
+
+func NewGraphQLClient(tokenData *TokenData, authData *AuthData) *GraphQLClient {
+	token, err := tokenData.GetToken(authData)
 	if err != nil {
 		log.Println("Failed to get token:", err)
 	}
 
 	return &GraphQLClient{
-		client: createGraphQLClient(token),
+		client: createGraphQLClient(token, authData),
 	}
 }
 
-func createGraphQLClient(token string) *graphql.Client {
+func createGraphQLClient(token string, authData *AuthData) *graphql.Client {
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
-	var client = graphql.NewClient(fmt.Sprintf("%s/graphql", os.Getenv("SERVER_URL")), httpClient)
+	var client = graphql.NewClient(fmt.Sprintf("%s/graphql", authData.ServerURL), httpClient)
 
 	return client
 }
@@ -67,7 +68,7 @@ func (authData *AuthData) Auth() (string, error) {
 		return "", fmt.Errorf("error while building the auth request: %v", err)
 	}
 
-	authEndpoint := fmt.Sprintf("%s/auth", os.Getenv("SERVER_URL"))
+	authEndpoint := fmt.Sprintf("%s/auth", authData.ServerURL)
 	res, err := http.Post(authEndpoint, "application/json", bytes.NewReader(marshalled))
 	if err != nil {
 		return "", fmt.Errorf("error while authenticating: %v", err)
@@ -95,10 +96,9 @@ func (authData *AuthData) Auth() (string, error) {
 	return tokenResponse.Token, nil
 }
 
-func (td *TokenData) GetToken() (string, error) {
+func (td *TokenData) GetToken(authData *AuthData) (string, error) {
 	if len(td.Token) == 0 || isTokenExpired(td.Token) {
-		auth := NewAuthData()
-		token, err := auth.Auth()
+		token, err := authData.Auth()
 		if err != nil {
 			log.Println("Authentication failed:", err)
 			return "", err
@@ -112,15 +112,15 @@ func (td *TokenData) GetToken() (string, error) {
 	return td.Token, nil
 }
 
+func (td *TokenData) setClientIdFromClaims(tokenString string) {
+	var claims = getClaimsFromToken(tokenString)
+	td.ClientId = claims.Subject
+}
+
 func isTokenExpired(tokenString string) bool {
 	var claims = getClaimsFromToken(tokenString)
 
 	return claims.Expiry.Time().Before(time.Now())
-}
-
-func (td *TokenData) setClientIdFromClaims(tokenString string) {
-	var claims = getClaimsFromToken(tokenString)
-	td.ClientId = claims.Subject
 }
 
 func getClaimsFromToken(tokenString string) jwt.Claims {
--- go.mod
+++ go.mod
@@ -5,14 +5,15 @@ go 1.22.1
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/robfig/cron/v3 v3.0.0
 	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466
+	github.com/traefik/yaegi v0.16.1
 	golang.org/x/oauth2 v0.18.0
 	gopkg.in/square/go-jose.v2 v2.6.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/robfig/cron/v3 v3.0.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
