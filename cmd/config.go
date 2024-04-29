ca12fffe99c1175c0fb775fda35ad74f2b8459a9
Denis Pojar <denis.pojar@trimm.nl>
2024-04-29 10:52:07 +0200

    feat(#8): move the service layer methods into a separate running instance

---
+++ cmd/config.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type AppConfig struct {
+	clientName   string
+	clientSecret string
+	serverURL    string
+	logFilePath  string
+	dbFilePath   string
+}
+
+func NewAppConfig() *AppConfig {
+	loadEnv()
+	return &AppConfig{
+		clientName:   os.Getenv("CLIENT_NAME"),
+		clientSecret: os.Getenv("CLIENT_SECRET"),
+		serverURL:    os.Getenv("SERVER_URL"),
+		logFilePath:  os.Getenv("LOG_FILE_PATH"),
+		dbFilePath:   os.Getenv("DB_FILE_PATH"),
+	}
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func dbConnect(file string) *sql.DB {
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		log.Fatal("Database connection issues: ", err)
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466
