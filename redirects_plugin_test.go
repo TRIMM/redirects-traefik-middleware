d29df3b851a9bbc2a6f7fab24ad89b91f705c252
Denis Pojar <denis.pojar@trimm.nl>
2024-06-21 14:31:08 +0200

    feat(testing): make benchmark and simple tests to use a mock server

--- redirects_plugin_test.go
+++ redirects_plugin_test.go
@@ -1,88 +1,134 @@
 package redirects_traefik_middleware
 
 import (
-	"context"
+	"fmt"
+	"github.com/TRIMM/redirects-traefik-middleware/internal/app"
+	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-const AppUrl = "http://localhost:8081"
-const AppName = "test-middleware"
+type TestRedirectStruct struct {
+	name             string
+	requestURL       string
+	expectedRedirect string
+}
 
-func TestServeHTTP_Match_Redirect(t *testing.T) {
+func getTestCases() *[]TestRedirectStruct {
+	return &[]TestRedirectStruct{
+		{
+			"Exact domain match redirect",
+			"https://old-domain.com",
+			"https://new-domain/post/laptop/clothing/",
+		},
+		{
+			"Exact relative path match redirect",
+			"http://example.com/product/furniture/electronics/",
+			"http://example.com/category/iphone/books/",
+		},
+	}
+}
+
+func startMockRedirectsServer(idx *app.IndexedRedirects) *httptest.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+			return
+		}
+
+		request := string(requestBody)
+		redirectURL, ok := idx.Match(request)
+		if !ok {
+			redirectURL = "@empty"
+		}
+		_, err = fmt.Fprintf(w, "%s", redirectURL)
+		if err != nil {
+			log.Println("Failed to write response:", err)
+		}
+	})
+
+	return httptest.NewServer(mux)
+}
+
+func getRedirectsPlugin(serverURL string) *RedirectsPlugin {
 	nextHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(http.StatusOK)
 	})
 
-	config := &Config{
-		RedirectsAppURL: AppUrl,
+	return &RedirectsPlugin{
+		redirectsAppURL: serverURL,
+		next:            nextHandler,
 	}
+}
 
-	handler, err := New(context.Background(), nextHandler, config, AppName)
-	if err != nil {
-		t.Fatalf("Failed to create middleware: %v", err)
-	}
+func TestServeHTTP_Match_Redirect(t *testing.T) {
+	idx := app.NewIndexedRedirects()
+	idx.IndexRule("", "old-domain.com", "https://new-domain/post/laptop/clothing/")
+	idx.IndexRule("/product/furniture/electronics/", "", "/category/iphone/books/")
 
-	testCases := []struct {
-		name             string
-		requestURL       string
-		expectedRedirect string
-	}{
-		{
-			name:             "Exact domain match redirect",
-			requestURL:       "https://fromsite2422.com",
-			expectedRedirect: "https://new-domain/post/laptop/clothing/",
-		},
-		{
-			name:             "Exact relative path match redirect",
-			requestURL:       "http://example.com/product/furniture/electronics/",
-			expectedRedirect: "http://example.com/category/iphone/books/",
-		},
-	}
+	mockServer := startMockRedirectsServer(idx)
+	defer mockServer.Close()
 
-	for _, tc := range testCases {
+	redirectsPlugin := getRedirectsPlugin(mockServer.URL)
+	testCases := getTestCases()
+
+	for _, tc := range *testCases {
 		t.Run(tc.name, func(t *testing.T) {
 
-			req, err := http.NewRequest("GET", tc.requestURL, nil)
+			req, err := http.NewRequest("GET", tc.requestURL, strings.NewReader(tc.requestURL))
 			if err != nil {
 				t.Fatalf("Failed to create request: %v", err)
 			}
 
 			rr := httptest.NewRecorder()
-			handler.ServeHTTP(rr, req)
+			redirectsPlugin.ServeHTTP(rr, req)
 
 			if rr.Code != http.StatusFound {
 				t.Errorf("handler returned wrong status code: got %v want %v",
 					rr.Code, http.StatusFound)
 			}
 
-			location, err := rr.Result().Location()
-			if err != nil {
-				t.Fatalf("failed to get Location header: %v", err)
-			}
-
-			if location.String() != tc.expectedRedirect {
-				t.Errorf("handler returned unexpected redirect URL: got %v want %v",
-					location.String(), tc.expectedRedirect)
+			location := rr.Header().Get("Location")
+			if location != tc.expectedRedirect {
+				t.Errorf("handler returned unexpected redirect URL: got %v want %v", location, tc.expectedRedirect)
 			}
 		})
 	}
 }
 
-func BenchmarkServeHTTP_Match_Redirect(b *testing.B) {
-	nextHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-	})
+func TestServeHTTP_NoMatch_Redirect(t *testing.T) {
+	idx := app.NewIndexedRedirects()
+	mockServer := startMockRedirectsServer(idx)
+	defer mockServer.Close()
 
-	config := &Config{
-		RedirectsAppURL: AppUrl,
-	}
-
-	handler, err := New(context.Background(), nextHandler, config, AppName)
+	redirectsPlugin := getRedirectsPlugin(mockServer.URL)
+	req, err := http.NewRequest("GET", "http://example.com/nonexistent", nil)
 	if err != nil {
-		b.Fatalf("Failed to create middleware: %v", err)
+		t.Fatal(err)
 	}
+
+	rr := httptest.NewRecorder()
+	redirectsPlugin.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func BenchmarkMiddleware_Match_Redirect(b *testing.B) {
+	idx := app.NewIndexedRedirects()
+	idx.IndexRule("/product/furniture/electronics/", "", "/category/iphone/books/")
+
+	mockServer := startMockRedirectsServer(idx)
+	defer mockServer.Close()
+
+	redirectsPlugin := getRedirectsPlugin(mockServer.URL)
 
 	req, err := http.NewRequest("GET", "http://example.com/product/furniture/electronics/", nil)
 	if err != nil {
@@ -93,75 +139,39 @@ func BenchmarkServeHTTP_Match_Redirect(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		rr := httptest.NewRecorder()
-		handler.ServeHTTP(rr, req)
+		redirectsPlugin.ServeHTTP(rr, req)
 
 		if rr.Code != http.StatusFound {
 			b.Errorf("handler returned wrong status code: got %v want %v",
 				rr.Code, http.StatusFound)
 		}
 
-		// Check the Location header for the redirect URL
 		expectedRedirectURL := "http://example.com/category/iphone/books/"
-		location, _ := rr.Result().Location()
-		if location.String() != expectedRedirectURL {
+		location := rr.Header().Get("Location")
+		if location != expectedRedirectURL {
 			b.Errorf("handler returned unexpected redirect URL: got %v want %v",
-				location.String(), expectedRedirectURL)
+				location, expectedRedirectURL)
 		}
 	}
 }
 
-func TestServeHTTP_NoMatch_Redirect(t *testing.T) {
-	nextHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-	})
+func BenchmarkMiddleware_NoMatch_Redirect(b *testing.B) {
+	idx := app.NewIndexedRedirects()
+	mockServer := startMockRedirectsServer(idx)
+	defer mockServer.Close()
 
-	config := &Config{
-		RedirectsAppURL: AppUrl,
-	}
-
-	handler, err := New(context.Background(), nextHandler, config, AppName)
-	if err != nil {
-		t.Fatalf("Failed to create middleware: %v", err)
-	}
-
-	req, err := http.NewRequest("GET", "http://example.com/nonexistent", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-}
-
-func BenchmarkServeHTTP_NoMatch_Redirect(b *testing.B) {
-	nextHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-	})
-
-	config := &Config{
-		RedirectsAppURL: AppUrl,
-	}
-
-	handler, err := New(context.Background(), nextHandler, config, AppName)
-	if err != nil {
-		b.Fatalf("Failed to create middleware: %v", err)
-	}
+	redirectsPlugin := getRedirectsPlugin(mockServer.URL)
 
 	req, err := http.NewRequest("GET", "http://example.com/nonexistent", nil)
 	if err != nil {
 		b.Fatalf("Failed to create request: %v", err)
 	}
 
-	b.ResetTimer() // Reset the timer to ignore the setup time
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		rr := httptest.NewRecorder()
-		handler.ServeHTTP(rr, req)
+		redirectsPlugin.ServeHTTP(rr, req)
 
 		if status := rr.Code; status != http.StatusOK {
 			b.Errorf("handler returned wrong status code: got %v want %v",
